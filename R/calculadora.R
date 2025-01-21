863a8aa95cc2bdfe936ed83e765775d21e5f3bb1
Janio Muianga <janiodrey@gmail.com>
2025-01-21 11:33:51 +0200

    first commit

---
+++ R/calculadora.R
@@ -0,0 +1,13 @@
+#' Soma
+#'
+#' Esta função calcula a soma de dois valores
+#'
+#' @param x - numero int
+#' @param y - numero int
+#' @export
+
+somar <- function(x, y){
+  soma <- x + y
+
+  return (soma)
+}
