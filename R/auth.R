578af09464c2da507470c41cc4c9801d480c96c8
Janio Muianga <janiodrey@gmail.com>
2025-02-11 11:48:19 +0200

    melhorias na função get acess

--- R/auth.R
+++ R/auth.R
@@ -1,17 +1,20 @@
+auth_env <- new.env()
 
-#' get_access_token
+auth_env$BASE_URL <- "https://accounts.zoho.com"
+
+#' Função para buscar o access token
 #'
 #' @description
-#' Retorna o access token do API Zoho
+#' O access token é usado para manipular dados no Zoho.
 #'
-#' @param client_id ID do Cliente Zoho
-#' @param client_secret secret code do Cliente Zoho
-#' @param refresh_token token para geração de access tokens
+#' @param client_id ID do Cliente
+#' @param client_secret secret code do Cliente
+#' @param refresh_token token usado para geração de access tokens
 #'
 #' @export
 get_access_token <- function(client_id, client_secret, refresh_token){
-  # composição da url
-  url <- glue("https://accounts.zoho.com/oauth/v2/token?refresh_token={refresh_token}&client_id={client_id}&client_secret={client_secret}&grant_type=refresh_token")
+  # url para fazer o refresh token
+  url <- glue("{auth_env$BASE_URL}/oauth/v2/token?refresh_token={refresh_token}&client_id={client_id}&client_secret={client_secret}&grant_type=refresh_token")
 
   # request the access token
   response_data <- request(url) |>
@@ -20,5 +23,6 @@ get_access_token <- function(client_id, client_secret, refresh_token){
     resp_body_json()
 
   # returns the access token
+  # TODO: retornar em quanto tempo expira o token
   return(response_data$access_token)
 }
